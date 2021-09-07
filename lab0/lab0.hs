e44562657b68da427d6ee7da8448df7e98f1a1bd
Maria Kanyushkova <33763743+maria-kanyushkova@users.noreply.github.com>
2021-09-07 18:09:55 +0300

    update lab0

--- lab0/lab0.hs
+++ lab0/lab0.hs
@@ -6,6 +6,9 @@ t_Fermat n = n + t_Fermat (n - 1)
 p_Fermat 1 = 1
 p_Fermat n = t_Fermat n + p_Fermat (n - 1)
 
+pyramidal :: Integer -> Integer
+pyramidal n = n * ( n + 1 ) * ( 2 * n + 1 ) `div` 6
+
 main = do
     putStr "N1:"
     print (snd(fst((1, 'a'), "abc")))
@@ -18,12 +21,14 @@ main = do
     putStr "N2:"
     print (head(head(tail([['a','d'], ['b', 'c']]))))
     putStr "N3:"
-    print ([1, 3 .. 20])
+    print ([1, 3 .. 40])
     putStr "N3:"
-    print ([ x * 2 + 1 | x <- [0 .. 9]])
+    print ([ x * 2 + 1 | x <- [0 .. 19]])
     putStr "N3:"
-    print ([ x | x <- [1 .. 20], odd x])
+    print ([ x | x <- [1 .. 40], odd x])
     putStr "N4:"
     print (map t_Fermat [1 .. 50])
+--    putStr "N5:"
+--    print (map p_Fermat [1 .. 50])
     putStr "N5:"
-    print (map p_Fermat [1 .. 50])
+    print ([pyramidal x | x <- [1..50]])
