acf34467576457e13fbd052b4ba713db6033f58c
Maria Kanyushkova <33763743+maria-kanyushkova@users.noreply.github.com>
2021-11-30 20:20:28 +0300

    update 9 lab

--- prolog/lab9/lab9_1.pl
+++ prolog/lab9/lab9_1.pl
@@ -52,28 +52,3 @@ aunt(X, Y) :- parent(Z, Y), sister(X, Z).
 f) Определите отношение "кузин"- cousin(X,Y).
 */
 cousin(X, Y) :- parent(Z, X), parent(A, Y), brother_or_sister(A, Z).
-
-/**
-a) Создать базу данных "Хобби".
-Предикат likes определяет отношение человек - хобби.
-*/
-likes(ellen, reading).
-likes(john, reading).
-likes(john, computers).
-likes(john, badminton).
-likes(john, photo).
-likes(leonardo, badminton).
-likes(eric, reading).
-likes(eric, swimming).
-likes(eric, chess).
-likes(paul, swimming).
-
-/**
-b) Составить вопрос и найти тех, кто имеет четыре хобби.
-*/
-four_hobbi(X) :- setof(Y, likes(X, Y), Result), length(Result, 4).
-
-/**
-c) Составить вопрос и найти тех, у кого одинаковые хобби.
-*/
-equal(X, Y) :- likes(X, Z), likes(Y, Z), different(X, Y).
